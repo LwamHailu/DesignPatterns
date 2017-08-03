05ca833b5c8f08b9f30e22257f98b6832393bdc7
Lwam Hailu <lwi25@yahoo.com>
2017-08-03 10:41:33 -0500

    //Strategy Pattern

---
+++ src/strategy/Client.java
@@ -0,0 +1,26 @@
+package strategy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Client {
+	public static void main(String[] args) {
+		//Flight flight=new Flight();
+		List<Flight>flight= new ArrayList<>();
+		flight.add(new Flight(233));
+		flight.add(new Flight(300));
+		flight.add(new Flight(200));
+		flight.add(new Flight(188));
+		FlightModel model= new FlightModel();
+		model.setFlightStrategy(new SinglePrice());
+		
+		System.out.println(model.getRevenue(flight));
+		model.setFlightStrategy(new TwoClasses());
+		System.out.println(model.getRevenue(flight));
+		model.setFlightStrategy(new MultiClass());
+		System.out.println(model.getRevenue(flight));
+		
+		
+	}
+
+}
